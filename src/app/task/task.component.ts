92be966021639e8bb74ccb8bfaf27fc45d02a5b6
Jesse Mazzella <jessemazzella@gmail.com>
2019-09-05 12:59:19 -0700

    make a task display

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
+  id: number = -1;
+  title: string = "Default task";
+  description: string = "Some boring task";
+  assigned: string = "Bubsy";
+  
   constructor() { }
-
-  ngOnInit() {
+  ngOnInit(){
   }
-
 }
