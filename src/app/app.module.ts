8c9e75272730e6d4ae68ae093d87a7d44dc9e16b
Canyon Schwartz <canyon.schwartz@gmail.com>
2019-09-18 11:28:09 -0700

    Added material input to toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
@@ -21,7 +22,8 @@ import {TaskStatusDetailComponent} from './task-status-detail/task-status-detail
   ],
   imports: [
     BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule,
-    MatDividerModule, MatListModule, MatToolbarModule, MatButtonModule
+    MatDividerModule, MatListModule, MatToolbarModule, MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
