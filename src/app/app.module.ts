2f5dcad50fb17be647a28d1f622b6c1f46e85898
Jesse Mazzella <jessemazzella@gmail.com>
2019-09-25 12:25:28 -0700

    implement basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,50 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
-import { FooterComponent } from './footer/footer.component';
-import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { CategoryDetailComponent } from './task-status-detail/category-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/board',
+    pathMatch: 'full'
+  },
+  {
+    path: 'board',
+    component: BoardComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/board',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent, BoardComponent, TaskDetailComponent,
-    CategoryDetailComponent, FooterComponent
+    AppComponent,
+    BoardComponent,
+    TaskDetailComponent,
+    CategoryDetailComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatCardModule,
-    MatDividerModule, MatIconModule, MatListModule, MatToolbarModule, MatButtonModule,
-    MatInputModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
