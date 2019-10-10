44a9ba89b90976b8ca37a797b8ffc2a8071ca45b
Canyon Schwartz <canyon.schwartz@gmail.com>
2019-10-10 13:54:24 -0700

    New data representation (#3)
    * Updated architecture
    
    * Some minor changes
    
    * Another minor change
    
    * Removed dead code

--- src/app/model/category.ts
+++ src/app/model/category.ts
@@ -3,12 +3,14 @@ import { Task } from './task';
 export interface ICategory {
     id: number;
     title: string;
+    tasks: Task[];
 }
 
 export class Category implements ICategory {
 
     id: number;
     title = '';
+    tasks = [];
 
     constructor(title: string, id: number) {
         this.id = id;
